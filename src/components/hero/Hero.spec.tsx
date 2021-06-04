b721f4c94a2989221298485ae2216122dd1a9e61
Fabien Schlegel <fabien.schlegel.fr@gmail.com>
2021-06-04 19:55:58 +0000

    Resolve "Add Hero"

---
+++ src/components/hero/Hero.spec.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable no-undef */
+import React from 'react';
+
+import '../../setupTests';
+
+import { mount } from 'enzyme';
+
+import Hero from './Hero';
+
+describe('Test Hero', () => {
+  const title = 'Lorem ipsum';
+  const subtitle = '';
+
+  it('Mount Hero', () => {
+    const wrapper = mount(<Hero title={title} subtitle={subtitle} />);
+    expect(wrapper.length).toEqual(1);
+  });
+
+  it('Hero is small', () => {
+    const wrapper = mount(<Hero title={title} subtitle={subtitle} size="is-small" />);
+
+    const messageDiv = wrapper.find('.hero').hasClass('is-small');
+
+    expect(messageDiv).toBeTruthy();
+  });
+
+  it('Hero has danger color', () => {
+    const wrapper = mount(<Hero title={title} subtitle={subtitle} color="is-danger" />);
+
+    const messageDiv = wrapper.find('.hero').hasClass('is-danger');
+
+    expect(messageDiv).toBeTruthy();
+  });
+
+  it('Hero has head and footer', () => {
+    const head = <p>Head of the Hero</p>;
+    const footer = <p>Footer of the Hero</p>;
+    const wrapper = mount(
+      <Hero title={title} subtitle={subtitle} head={head} footer={footer} />
+    );
+
+    const headDiv = wrapper.find('.hero-head').exists();
+    const footerDiv = wrapper.find('.hero-foot').exists();
+
+    expect(headDiv).toBeTruthy();
+    expect(footerDiv).toBeTruthy();
+  });
+});
