1b23f07f0c9f78291661e752dc111b545a7137a4
Fabien Schlegel <fabien.schlegel.fr@gmail.com>
2021-07-12 06:46:21 +0000

    Upgrade to 0.4.1

---
+++ src/components/checkbox/Checkbox.spec.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+import React from 'react';
+
+import '../../setupTests';
+
+import { mount } from 'enzyme';
+
+import Checkbox from './Checkbox';
+
+describe('Test Checkbox', () => {
+  it('Mount Checkbox', () => {
+    const wrapper = mount(<Checkbox>Checkbox Content</Checkbox>);
+    expect(wrapper.length).toEqual(1);
+  });
+
+  it('Test Checkbox is disabled', () => {
+    const wrapper = mount(<Checkbox isDisabled>Text Content</Checkbox>);
+
+    const disabledInput = wrapper.find({ disabled: true });
+
+    expect(disabledInput).toBeTruthy();
+  });
+});
