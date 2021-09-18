223add5e8849c39a309978da266de7e1a1a4fc15
Fabien Schlegel <fabien.schlegel.fr@gmail.com>
2021-09-18 10:29:06 +0200

    Add className to card content

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -9,16 +9,24 @@ import { CardFooterProps } from '../card-footer/CardFooter';
 export interface CardProps {
   children: ReactNode;
   className?: string;
+  contentClassName?: string;
   header?: ReactElement<CardHeaderProps>;
   image?: ReactElement<CardImageProps>;
   footer?: ReactElement<CardFooterProps>;
 }
 
-const Card: React.FC<CardProps> = ({ children, className, header, image, footer }) => (
+const Card: React.FC<CardProps> = ({
+  children,
+  className,
+  contentClassName,
+  header,
+  image,
+  footer,
+}) => (
   <div className={clsx('card', className)}>
     {header}
     {image}
-    <div className="card-content">{children}</div>
+    <div className={clsx('card-content', contentClassName)}>{children}</div>
     {footer}
   </div>
 );
